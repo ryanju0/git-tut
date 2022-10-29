39d1e3e709cffe1a7159947eafaef089904f62b8
wetyuiopsu <51488420+wetyuiopsu@users.noreply.github.com>
2022-10-29 10:43:59 -0500

    add intakesetcmd and intakesubsystem

--- cmdproj/cmdproj/src/main/java/frc/robot/RobotContainer.java
+++ cmdproj/cmdproj/src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import frc.robot.subsystems.ClimbSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotController;
+import frc.robot.commands.IntakeSetCmd;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
